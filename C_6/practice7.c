f45f01c6a5846980096803d454db1e173d97c169
zzingni <128567594+zzingni@users.noreply.github.com>
2023-04-06 16:08:01 +0900

    반복문
    C프로그래밍_반복문 활용_숫자 출력

---
+++ C_6/practice7.c
@@ -0,0 +1,29 @@
+// 반복문 활용하여 반복횟수 입력받아 두 수의 합 출력하기 // 
+
+#include <stdio.h>
+#define _CRT_SECURE_NO_WARNINGS
+
+int main(void)
+{
+
+    int x, y, n, sum;
+    int i = 0;
+
+    printf("반복횟수를 설정하시오: ");
+    scanf("%d", &n);
+
+    while ( i < n) 
+    {
+    printf("두 수를 입력하시오: ");
+    scanf("%d %d", &x, &y);
+    sum = x + y;
+    i++;
+    printf("%d\n", sum);
+   
+    }
+
+    printf("종료되었습니다");
+    
+    return 0;
+
+}
