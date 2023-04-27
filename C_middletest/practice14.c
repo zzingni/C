375eafd545d1a0c6f22cc08eb1a787c153384479
zzingni <128567594+zzingni@users.noreply.github.com>
2023-04-27 16:02:37 +0900

    Create practice14.c
    증감 연산자

---
+++ C_middletest/practice14.c
@@ -0,0 +1,24 @@
+// 증감 연산자
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#define TAX_RATE 0.2
+
+int main(void)
+{
+ 
+    int x = 10;
+    int y = 10;
+
+    printf("x=%d \n", x);
+    printf("++의 값=%d \n", ++x);
+    printf("x=%d \n\n", x); 
+
+    printf("y=%d \n", y);
+    printf("y++의 값=%d \n", y++);
+    printf("y=%d \n",y);
+
+    return 0;
+
+
+}
