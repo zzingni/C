61a9d779e699cd8f0127e8f18d180d858b12744d
zzingni <128567594+zzingni@users.noreply.github.com>
2023-05-11 14:56:16 +0900

    난수 값 발생시키기

---
+++ C_11/practice1.c
@@ -0,0 +1,21 @@
+// 난수 값 이용하여 로또 번호 출력
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define MAX 45
+
+int main(void)
+{
+    int i;
+
+    srand( (unsigned) time (NULL));
+    for(i=0; i<6; i++)
+    {
+        printf("%d ", 1+rand()%MAX);
+    }
+
+    return 0;
+
+}
