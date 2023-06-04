954e8a6e48689a0f760bc4f93ed6be7c85e07c10
zzingni <128567594+zzingni@users.noreply.github.com>
2023-06-04 20:29:53 +0900

    function_HappyBirthday

---
+++ programming_practice/freshman_exam2/practice2.c
@@ -0,0 +1,18 @@
+// 생일 축하 함수 작성하여 출력 //
+
+#include <stdio.h>
+
+void HappyBirthday();
+
+int main(void)
+{
+    HappyBirthday();
+
+    return 0;
+}
+
+void HappyBirthday() {
+    printf("생일축하 합니다! \n");
+    printf("생일축하 합니다! \n");
+    printf("사랑하는 친구의 생일축하 합니다! \n");
+}
