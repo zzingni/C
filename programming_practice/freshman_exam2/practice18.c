a46a52856f51fe9258dfd28877e1f09f021a77e6
zzingni <128567594+zzingni@users.noreply.github.com>
2023-06-05 02:55:05 +0900

    function_saveamount

---
+++ programming_practice/freshman_exam2/practice18.c
@@ -0,0 +1,32 @@
+// 은행 계좌 구현하기
+
+#include <stdio.h>
+
+int main(void) {
+    printf("=====================================\n");
+    printf("입금 \t출금\t 잔고\n");
+    printf("=====================================\n");
+    save(10000);
+    save(50000);
+    save(-10000);
+    save(30000);
+    printf("=====================================\n");
+
+    return 0;
+}
+
+void save(int amount) {
+    static long balance = 0;
+
+    if(amount>=0)
+    {
+        printf("%d\t\t", amount);
+    }
+    else
+    {
+        printf("\t%d\t", amount);
+    }
+
+    balance += amount;
+    printf("%d \n", balance);
+}
