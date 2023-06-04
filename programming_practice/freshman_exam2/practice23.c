a1919be8d42cc6eeb305e0ddccb8123937f0ecb4
jieun gwon <128567594+zzingni@users.noreply.github.com>
2023-06-05 04:14:12 +0900

    array_basic

---
+++ programming_practice/freshman_exam2/practice23.c
@@ -0,0 +1,21 @@
+// 배열 기초 예제
+
+#include <stdio.h>
+
+int main(void) {
+    int i;
+    int scores[5];
+
+    scores[0] = 10;
+    scores[1] = 20;
+    scores[2] = 30;
+    scores[3] = 40;
+    scores[4] = 50;
+
+    for(i=0; i<5; i++)
+    {
+        printf("scores[%d]=%d\n",i, scores[i]);
+    } 
+
+    return 0;
+}
